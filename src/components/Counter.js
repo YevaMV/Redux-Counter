c17b4dec6a531375091f938c0d0f805960b0bd61
YevaMV <eva280988@gmail.com>
2022-07-04 20:48:33 +0400

    work with multiple slices

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -4,9 +4,10 @@ import classes from './Counter.module.css';
 
 const Counter = () => {
   const dispatch = useDispatch();
-  const counter = useSelector((state) => state.counter);
+  const counter = useSelector((state) => state.counter.counter);
 
-  const show = useSelector((state) => state.showCounter);
+  const show = useSelector((state) => state.counter.showCounter);
+
   const toggleCounterHandler = () => {
     dispatch(counterActions.toggelCounter());
   };
@@ -29,7 +30,7 @@ const Counter = () => {
       {show && <div className={classes.value}>{counter}</div>}
       <div>
         <button onClick={incrementHandler}>Increment</button>
-        <button onClick={increaseHandler}>Increase by 5</button>
+        <button onClick={increaseHandler}>Increase by 10</button>
         <button onClick={decrementtHandler}>Decrement</button>
       </div>
       <button onClick={toggleCounterHandler}>Toggle Counter</button>
